7435d4cc6c4423db60272a5e7a05e783f8720b00
awais512 <raza.awais25@gmail.com>
2021-01-06 14:45:24 +0500

    Generate password reset token

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
 
 const userSchema = new mongoose.Schema(
   {
@@ -63,6 +64,23 @@ userSchema.methods.getJwtToken = function () {
 //Complete Password
 userSchema.methods.comparePassword = async function (enterdPassword) {
   return await bcrypt.compare(enterdPassword, this.password);
+};
+
+//Generate password reset token
+userSchema.methods.getResetPasswordToken = function () {
+  //Generate Token
+  const resetToken = crypto.randomBytes(20).toString('hex');
+
+  //Hash and set resetPasswordToken
+  this.resetPasswordToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  //Set token expire time
+  this.resetPasswordExpire = Date.now() + 30 * 60 * 1000;
+
+  return resetToken;
 };
 
 module.exports = mongoose.model('User', userSchema);
