58bdc28c066a10bfd2a5cf9cacb01fa99c6624a1
awais512 <raza.awais25@gmail.com>
2021-03-19 15:15:13 +0500

    Get order by id and by its user

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -30,3 +30,38 @@ exports.newOrder = asyncHandler(async (req, res, next) => {
 
   res.status(200).json({ success: true, order });
 });
+
+//@desc     Get Single Order
+//@route    POST /api/v1/orders/:id
+//@access   Private
+exports.getOrder = asyncHandler(async (req, res, next) => {
+  const order = await Order.findById(req.params.id).populate(
+    'User',
+    'name email'
+  );
+
+  if (!order) {
+    return next(new ErrorHandler('No order found with this id', 404));
+  }
+
+  res.status(200).json({ success: true, order });
+});
+
+//@desc     Get LoggedIn user Order
+//@route    GET /api/v1/orders/me
+//@access   Private
+// exports.getLoggedInUserOrder = asyncHandler(async (req, res, next) => {
+//     const orders = await Order.find({ user: req.user.id });
+
+//     res.status(200).json({ success: true, orders });
+// });
+
+exports.getLoggedInUserOrder = async (req, res, next) => {
+  try {
+    const orders = await Order.find({ user: req.user.id });
+
+    res.status(200).json({ success: true, orders });
+  } catch (err) {
+    console.log(err);
+  }
+};
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-const { newOrder } = require('../controllers/orderController');
+const {
+  newOrder,
+  getOrder,
+  getLoggedInUserOrder,
+} = require('../controllers/orderController');
 const {
   isAuthenticated,
   authorizeRole,
 } = require('../middlewares/authMiddleware');
 
 router.route('/').post(isAuthenticated, newOrder);
+router.route('/:id').get(isAuthenticated, getOrder);
+router.route('/myorder/me').get(isAuthenticated, getLoggedInUserOrder);
 
 module.exports = router;
